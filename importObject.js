7661776ca3c16aeb832a3ca5c39e4d7c92fb59ae
durgasaikalyan <durgasaikalyanccbp@gmail.com>
2023-04-07 14:42:38 +0000

    your message

---
+++ importObject.js
@@ -0,0 +1,6 @@
+// import the default export
+const myObject = require("./exportObject");
+
+// use the default export
+console.log(myObject.firstName); // Output: John
+console.log(myObject.lastName); // Output: 30
